938ec5c0a38ee67d994e5024de706abaeddd8d23
Siavash Sefidrodi <siavash.sefidrodi@pressrelations.de>
2016-06-03 11:04:49 +0200

    fixed model and input data format

--- mixed_embeddings_reuters.py
+++ mixed_embeddings_reuters.py
@@ -2,11 +2,11 @@ from sklearn.base import TransformerMixin, BaseEstimator
 from keras.preprocessing import sequence
 from collections import Counter
 from keras.models import Model
-from keras.layers import Dense, Embedding, Dropout, merge, Merge, Input, \
-    TimeDistributed, Activation, Reshape, Permute, LSTM
+from keras.layers import Dense, Embedding, Input, TimeDistributed, LSTM,  \
+    Activation
 import numpy as np
 from keras.datasets import reuters
-
+from keras.utils import np_utils
 
 UNKNOWN_ITEM_ID = 0
 UNKNOWN_ITEM = u'\uFFFD'
@@ -40,6 +40,7 @@ class DataAccess(object):
         self.tokenizer = Tokenizer(vocab_size=vocab_size)
         self.padder = SequencePadder(max_word_len=max_word_length,
                                  max_text_len=max_text_length)
+        self.max_text_length = max_text_length
 
     def tokenize(self, X_test, X_train):
         vocab_char_size = self.tokenizer.fit(X_train + X_test)
@@ -56,17 +57,21 @@ class DataAccess(object):
         return X_test, X_train
 
     def load_data(self):
-        (X_train, y_train), (X_test, y_test) = reuters.load_data(start_char=None, index_from=None)
+        (X_train, y_train), (X_test, y_test) = reuters.load_data(
+            start_char=None, index_from=None, nb_words=self.max_text_length)
         index_word = dict((v, k) for k, v in reuters.get_word_index().items())
         X_train = [[index_word[idx] for idx in x] for x in X_train]
         X_test = [[index_word[idx] for idx in x] for x in X_test]
         X_test, X_train, vocab_char_size = self.tokenize(X_test, X_train)
         X_test, X_train = self.pad(X_test, X_train)
+        nb_classes = np.max(y_train)+1
+        Y_train = np_utils.to_categorical(y_train, nb_classes)
+        Y_test = np_utils.to_categorical(y_test, nb_classes)
         print(len(X_train), 'train sequences')
         print(len(X_test), 'test sequences')
         print('X_train shape:', X_train.shape)
         print('X_test shape:', X_test.shape)
-        return X_train, y_train, X_test, y_test, vocab_char_size
+        return X_train, Y_train, X_test, Y_test, vocab_char_size, nb_classes
 
 
 class CharIndexer(object):
@@ -151,19 +156,25 @@ class Tokenizer(BaseEstimator, TransformerMixin):
         return summary
 
 
-def build_model(batch_size, chars_vocab_size, word_count, word_length):
+def build_model(nb_classes, chars_vocab_size, word_count, word_length):
     print('Build model...')
-    input = Input(shape=(word_count, word_length),dtype='int32')
+    input = Input(shape=(word_count, word_length,),dtype='int32',
+                  name='input')
     embedded = TimeDistributed(Embedding(chars_vocab_size, 128,
-                                         input_length=word_count))(input)
-    forward_lstm = TimeDistributed(LSTM(64))(embedded)
+                                         input_length=word_count,
+                                         name='embedding'),
+                               name='td_embedding')(input)
+    forward_lstm = TimeDistributed(LSTM(64,name='char_lstm'),
+                                   name='td_char_lstm')(embedded)
     # backward_lstm = LSTM(64, go_backwards=True,
     #                      sequence_of_sequences=True)(input)
     # char_embedding = merge([forward_lstm,backward_lstm],mode='concat')
-    lstm = LSTM(64)(forward_lstm)
-    output = Dense(1, activation='sigmoid')(lstm)
+    lstm = LSTM(64, name='word_lstm')(forward_lstm)
+    dense = Dense(nb_classes, activation='sigmoid', name='dense')(lstm)
+    output = Activation('softmax', name='output')(dense)
     model = Model(input=input, output=output)
-    model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam',
+                  metrics=['accuracy'])
     return model
 
 
@@ -176,7 +187,7 @@ def train_and_test_model(X_train, y_train, X_test, y_test, batch_size, nb_epoch 
     print('Test accuracy:', acc)
 
 WORD_VOCAB_SIZE = 20000
-WORD_COUNT = 80
+WORD_COUNT = 1000
 WORD_LENGTH = 20
 BATCH_SIZE = 32
 CHAR_VOCAB_SIZE = 40
@@ -184,9 +195,9 @@ CHAR_VOCAB_SIZE = 40
 data_access = DataAccess(vocab_size=WORD_VOCAB_SIZE, max_word_length=WORD_LENGTH,
                   max_text_length=WORD_COUNT, )
 
-X_train, y_train, X_test, y_test, vocab_char_size = data_access.load_data()
+X_train, y_train, X_test, y_test, vocab_char_size, nb_classes = data_access.load_data()
 
-model = build_model(batch_size=BATCH_SIZE,
+model = build_model(nb_classes=nb_classes,
                     chars_vocab_size=CHAR_VOCAB_SIZE,
                     word_count=WORD_COUNT,
                     word_length=WORD_LENGTH)
